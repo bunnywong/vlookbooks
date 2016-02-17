934c4a3b23acd3f96c411d2509939e5a15beefd7
BunnY <w@bunnywong.com>
2016-02-17 20:33:36 +0800

    split to custom header

--- wp-content/themes/Parallax-One-child/template-home.php
+++ wp-content/themes/Parallax-One-child/template-home.php
@@ -5,40 +5,8 @@
  * @package parallax-one
  */
 
-	get_header();
+  get_header('home');
 ?>
-
-	</div>
-	<!-- /END COLOR OVER IMAGE -->
-</header>
-<div class="container-fluid custom-header">
-  <div class="menu">
-      <button type="button" class="navbar-toggle js-menu-io">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-    <span><a href="#" class="js-menu-io">Menu</a></span>
-  </div>
-  <h1>VLOOKBOOKS.COM</h1>
-
-  <div class="signup account js-signup">
-    <a href="#" class="btn btn-primary" data-toggle="modal" data-target=".modal-signup">Sign up</a>
-  </div>
-  <div class="login account">
-    <a href="#" class="btn btn-primary js-login" data-toggle="modal" data-target=".modal-login">Log in</a>
-  </div>
-
-</div>
-<div class="side-menu js-side-menu hidden">
-  <div class="menu">
-    <a href="/about">About</a>
-  </div>
-  <div class="menu">
-    <a href="/contact">Contact</a>
-  </div>
-</div>
-<!-- /END HOME / HEADER  -->
 
 <div class="content-wrap">
 	<div class="container">
