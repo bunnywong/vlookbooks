4ef7b2929e7de38f7d1bd47f2629ce658f752295
BunnY <w@bunnywong.com>
2016-03-23 23:26:17 +0800

    update repeater template

--- wp-content/plugins/ajax-load-more/core/repeater/default.php
+++ wp-content/plugins/ajax-load-more/core/repeater/default.php
@@ -1,11 +1,12 @@
 <div<?php if (! has_post_thumbnail() ) { echo ' class="no-img item-wrapper"'; }else{echo ' class="item-wrapper"';} ?>>
-   <?php if ( has_post_thumbnail() ) { 
-      the_post_thumbnail(array(150,150));
-   }?>
-   <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-   <p class="entry-meta">
-       <?php the_time("F d, Y"); ?>
-   </p>
-   <?php the_excerpt(); ?> 
-<?php echo do_shortcode('[simple-comments post_id="'.get_the_ID().'" style="default" get="20" border="true" order="DESC" auto_load="true"]'); ?>
+
+  <div class="info"><strong><?php the_author(); ?></strong><br>@<?php the_time("F d, Y"); ?></div>
+
+   <div class="content">
+     <?php if ( has_post_thumbnail() ) {
+        the_post_thumbnail(array(150,150));
+     }?>
+    <?php the_excerpt(); ?>
+  </div>
+  <?php echo do_shortcode('[simple-comments post_id="'.get_the_ID().'" style="default" get="20" border="true" order="DESC" auto_load="true"]'); ?>
 </div>
