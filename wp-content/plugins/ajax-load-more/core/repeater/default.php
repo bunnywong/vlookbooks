97bdc5626c350e2e0ea9a700a712c7bb6d8d18df
BunnY <w@bunnywong.com>
2016-03-23 21:57:41 +0800

    update ajax-load-more template

--- wp-content/plugins/ajax-load-more/core/repeater/default.php
+++ wp-content/plugins/ajax-load-more/core/repeater/default.php
@@ -1,9 +1,11 @@
-<li<?php if (! has_post_thumbnail() ) { echo ' class="no-img"'; } ?>>
-   <?php if ( has_post_thumbnail() ) { the_post_thumbnail(array(150,150));
+<div<?php if (! has_post_thumbnail() ) { echo ' class="no-img item-wrapper"'; }else{echo ' class="item-wrapper"';} ?>>
+   <?php if ( has_post_thumbnail() ) { 
+      the_post_thumbnail(array(150,150));
    }?>
-   <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
+   <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
    <p class="entry-meta">
        <?php the_time("F d, Y"); ?>
    </p>
-   <?php the_excerpt(); ?>
-</li>
+   <?php the_excerpt(); ?> 
+<?php echo do_shortcode('[simple-comments post_id="'.get_the_ID().'" style="default" get="20" border="true" order="DESC" auto_load="true"]'); ?>
+</div>
